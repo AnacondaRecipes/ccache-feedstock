affd5635f159ceea13ea8a2e57a98ed39c7679dd
Serhii Kupriienko <skupriienko@anaconda.com>
2021-05-27 15:16:10 +0300

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,15 +1,24 @@
 #!/bin/bash
+# Get an updated config.sub and config.guess
+cp $BUILD_PREFIX/share/gnuconfig/config.* .
 
-if [[ ${target_platform} == osx-64 ]]; then
-  export SDKROOT=${CONDA_BUILD_SYSROOT}
+mkdir build
+cd build
+
+if [ "$target_platform" = "osx-arm64" ]; then
+    # Workaround for wrong detection of vector extensions,
+    # see https://github.com/ccache/ccache/issues/734
+    CMAKE_ARGS="${CMAKE_ARGS} -DHAVE_AVX2=NO -DHAVE_C_AVX2=NO -DHAVE_C_AVX512=NO -DHAVE_C_SSE2=NO -DHAVE_C_SSE41=NO"
 fi
-./ccache.ccache/configure --prefix=$PREFIX
+
+cmake \
+    ${CMAKE_ARGS} \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DCMAKE_INSTALL_PREFIX=${PREFIX} \
+    ${SRC_DIR}
+
 make -j${CPU_COUNT}
-if [[ ${target_platform} != osx-64 ]]; then
-  # Disabling macOS due to:
-  # Test suite:     debug_prefix_map
-  # Test case:      Multiple -fdebug-prefix-map
-  # Failure reason: Relocation (name) not found in test.o
-  make check
+if [[ "${CONDA_BUILD_CROSS_COMPILATION}" != "1" ]]; then
+    make check
 fi
 make install
